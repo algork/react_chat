513b79ef59ac58805484c8fdb3aba041d237fdf9
algork <algork123@gmail.com>
2023-05-16 08:35:33 +0800

    changed chat field style and added list of chats

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,9 @@ import { MessageList } from "./Components/MessageList/MessageList";
 
 function App({ name, text }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h3>Hello, {name}</h3>
-        Send Us a Message
-        <Counter />
-        <MessageList />
-      </header>
+    <div>
+      <Counter />
+      <MessageList />
     </div>
   );
 }
--- src/Components/MessageList/MessageList.jsx
+++ src/Components/MessageList/MessageList.jsx
@@ -1,6 +1,14 @@
 import { useEffect, useRef, useState } from "react";
+import Box from "@mui/material/Box";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
 import IconButton from "@mui/material/IconButton";
 import SendIcon from "@mui/icons-material/Send";
+import ListItemText from "@mui/material/ListItemText";
+import DraftsIcon from "@mui/icons-material/Drafts";
+import InboxIcon from "@mui/icons-material/Inbox";
 
 import "./MessageList.css";
 
@@ -57,36 +65,59 @@ export const MessageList = () => {
   }, [messageList]);
 
   return (
-    <div className="chat-wrapper">
-      <div className="chat-field">
-        {messageList.map((msg) => (
-          <div className="message-div" key={msg.id}>
-            <div
-              className="avatar-wrapper"
-              style={{
-                flexDirection: msg.author === "Alex" ? "row-reverse" : "row",
-              }}
-            >
-              <div>
-                <img className="avatar" src={msg.image} alt="avatar" />
+    <div className="chat-window">
+      <Box sx={{ width: "100%", maxWidth: 360, bgcolor: "background.paper" }}>
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <InboxIcon />
+              </ListItemIcon>
+              <ListItemText primary="Inbox" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <DraftsIcon />
+              </ListItemIcon>
+              <ListItemText primary="Drafts" />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </Box>
+      <div className="chat-wrapper">
+        <div className="chat-field">
+          {messageList.map((msg) => (
+            <div className="message-div" key={msg.id}>
+              <div
+                className="avatar-wrapper"
+                style={{
+                  flexDirection: msg.author === "Alex" ? "row-reverse" : "row",
+                }}
+              >
+                <div>
+                  <img className="avatar" src={msg.image} alt="avatar" />
+                </div>
+                <div className="author-text-font">{msg.author}</div>
               </div>
-              <div className="author-text-font">{msg.author}</div>
+              <div className="message-font">{msg.text}</div>
             </div>
-            <div className="message-font">{msg.text}</div>
-          </div>
-        ))}
-      </div>
-      <div className="input-div">
-        <input ref={inputRef} />
-        <IconButton
-          onClick={() => sendMessage()}
-          color="primary"
-          aria-label="send"
-          size="small"
-          disableRipple
-        >
-          <SendIcon />
-        </IconButton>
+          ))}
+        </div>
+        <div className="input-div">
+          <form>
+            <input className="input" ref={inputRef} />
+            <IconButton
+              onClick={() => sendMessage()}
+              color="primary"
+              aria-label="send"
+              size="large"
+            >
+              <SendIcon />
+            </IconButton>
+          </form>
+        </div>
       </div>
     </div>
   );
